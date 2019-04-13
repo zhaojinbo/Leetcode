5499570ccf22fdc2f9db3c3864d9dde0db4e1de7
zhaojinbo <zhaojb1995@163.com>
2019-04-13 18:52:41 +0800

    修改jd文件夹下的文件

---
+++ src/jd/Main.java
@@ -0,0 +1,14 @@
+package jd;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()){
+            int n = scanner.nextInt();
+
+
+        }
+    }
+}
