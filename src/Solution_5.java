a5c2d94d69176244e46c0872d4c8ef7081884323
zhaojinbo <zhaojb1995@163.com>
2019-04-24 15:29:33 +0800

    improve Solution_5

--- src/Solution_5.java
+++ src/Solution_5.java
@@ -1,5 +1,11 @@
 public class Solution_5 {
-    public String longestPalindrome(String s) {
+    /**
+     * 动态代理方法来做，时间复杂度为O(n^2)，103ms
+     *
+     * @param s
+     * @return
+     */
+    public String longestPalindromeOne(String s) {
         int len = s.length();
         if (len < 2) {
             return s;
@@ -30,5 +36,45 @@ public class Solution_5 {
             }
         }
         return s.substring(start, end);
+    }
+
+    /**
+     * 中心扩展法，速度飞起，最快5ms
+     *
+     * @param s
+     * @return
+     */
+    public static String longestPalindrome(String s) {
+        int len = s.length();
+        if (len < 2) {
+            return s;
+        }
+        int[] range = new int[2];
+        char[] chars = s.toCharArray();
+        int low;
+        int high;
+        for (int i = 0; i < chars.length; i++) {
+            low = i;
+            high = low;
+            while (high < chars.length - 1 && chars[high + 1] == chars[low]) {
+                high++;
+            }
+            // 中间一个细节，以供优化，当中将连续几个字符相等时，直接跳到这段连续相等字符的最后
+            i = high;
+            while (low > 0 && high < chars.length - 1 && chars[low - 1] == chars[high + 1]) {
+                low--;
+                high++;
+            }
+            if (high - low > range[1] - range[0]) {
+                range[0] = low;
+                range[1] = high;
+            }
+        }
+        return s.substring(range[0], range[1] + 1);
+    }
+
+    public static void main(String[] args) {
+        String res = longestPalindrome("babad");
+        System.out.println(res);
     }
 }
