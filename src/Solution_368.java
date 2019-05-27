002cad3849c7d69f1e66500bb30d1ac3e3c0f933
zhaojinbo <zhaojb1995@163.com>
2019-05-27 10:17:16 +0800

    improve Solution_368

--- src/Solution_368.java
+++ src/Solution_368.java
@@ -8,36 +8,26 @@ public class Solution_368 {
         if (length == 0) {
             return new ArrayList<>();
         }
-        if (length == 1) {
-            List<Integer> list = new ArrayList<>();
-            list.add(nums[0]);
-            return list;
-        }
         Arrays.sort(nums);
         int[] dp = new int[length];//记录以当前元素结尾的最长子序列长度
         int[] prePosition = new int[length];//记录当前元素能够整除的前面一个元素的位置
-        dp[0] = 1;
-        int max = 1;
-        int max_index = 0;
+        for (int i = 0; i < length; i++) {
+            dp[i] = 1;//先将所有值都初始化为1
+        }
+        int max = 1;//最长子序列的长度
+        int max_index = 0;//最长子序列是以哪个元素结尾的
         for (int i = 1; i < length; i++) {
-            boolean flag = true;
-            int temp;
             for (int j = 0; j < i; j++) {
                 if (nums[i] % nums[j] == 0) {
-                    temp = dp[j] + 1;
-                    if (temp > dp[i]) {
-                        dp[i] = temp;
-                        if (dp[i] > max) {
-                            max = dp[i];
-                            max_index = i;
-                        }
+                    if (dp[j] + 1 > dp[i]) {
+                        dp[i] = dp[j] + 1;
                         prePosition[i] = j;
                     }
-                    flag = false;
                 }
             }
-            if (flag) {
-                dp[i] = 1;
+            if (dp[i] > max) {
+                max = dp[i];
+                max_index = i;
             }
         }
         List<Integer> res = new ArrayList<>();
