37ff6d841b14c4f5a8bf6f7b3c5d9cbbcb6be4b8
zhaojinbo <zhaojb1995@163.com>
2019-06-25 23:37:13 +0800

    为Solution_93添加注解

--- src/Solution_93.java
+++ src/Solution_93.java
@@ -9,7 +9,7 @@ public class Solution_93 {
             return res;
         }
         int length = s.length();
-        if (length < 4) {
+        if (length < 4 || length > 12) {
             return res;
         }
         LinkedList<String> templist = new LinkedList<>();
@@ -17,23 +17,30 @@ public class Solution_93 {
         return res;
     }
 
+    /**
+     * 回溯算法，每添加一个点，算是解空间路径中的一层
+     * @param res 结果集
+     * @param templist 用来存放中间结果的list，需要在每一步路径回退的时候移除最后添加进去的string
+     * @param prePosition 前一个点放置的位置，初始值为-1
+     * @param dotnum 当前需要方式的点的个数
+     */
     private void dfs(ArrayList<String> res, LinkedList<String> templist, String s, int length, int prePosition, int dotnum) {
         int minPosition = Math.min(length - 2, prePosition + 3);
         for (int curPosition = prePosition + 1; curPosition <= minPosition; curPosition++) {
             String substring = s.substring(prePosition + 1, curPosition + 1);
             if (valid(substring)) {
                 templist.add(substring);
-                if (dotnum == 1) {
+                if (dotnum == 1) {//当放置完最后一个点的时候，还需要将最后一段添加进去
                     String substring1 = s.substring(curPosition + 1, length);
-                    if (valid(substring1)){
+                    if (valid(substring1)) {
                         templist.add(substring1);
                         res.add(String.join(".", templist));
-                        templist.removeLast();
+                        templist.removeLast();//回溯算法中回退需要的步骤
                     }
                 } else {
                     dfs(res, templist, s, length, curPosition, dotnum - 1);
                 }
-                templist.removeLast();
+                templist.removeLast();//回溯算法中回退需要的步骤
             }
         }
     }
