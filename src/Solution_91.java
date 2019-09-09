d700c167b770fce0edf3a770b6a892fdfa6f9352
zhaojinbo <zhaojb1995@163.com>
2019-09-09 14:52:01 +0800

    improve Solution_91

--- src/Solution_91.java
+++ src/Solution_91.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Solution_91 {
     public int numDecodings(String s) {
         int length = s.length();
@@ -54,5 +56,47 @@ public class Solution_91 {
             }
         }
         return count[length - 1];
+    }
+
+    /**
+     * 下面是对上面题目的延伸，上面是求出解码的个数，下面是求出解码的所有情况
+     * 所以上面是动态规划，下面是回溯算法
+     *
+     * @param num 解码前的数字组成的字符串
+     */
+    public void detailDecoding(String num) {
+        ArrayList<StringBuilder> list = new ArrayList<>();
+        StringBuilder stringBuilder = new StringBuilder();
+        decode(num, 0, stringBuilder, list);
+        for (StringBuilder str : list) {
+            System.out.println(str.toString());
+        }
+        System.out.println(list.size());
+    }
+
+    /**
+     * @param num           解码前的数字组成的字符串
+     * @param start         从start下标位置开始向后1或者2个字符
+     * @param stringBuilder 进入递归的初始str
+     * @param list          结果集
+     */
+    private static void decode(String num, int start, StringBuilder stringBuilder, ArrayList<StringBuilder> list) {
+        int length = num.length();
+        if (start >= length) {
+            list.add(stringBuilder);
+            return;
+        }
+        char c = num.charAt(start);
+        if (c == '0') {
+            return;
+        }
+        stringBuilder.append((char) ('A' + Integer.parseInt(num.substring(start, start + 1)) - 1));
+        decode(num, start + 1, new StringBuilder(stringBuilder), list);
+        stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+
+        if (start + 1 < length && (c == '1' || (c == '2' && num.charAt(start + 1) < '7'))) {
+            stringBuilder.append((char) ('A' + Integer.parseInt(num.substring(start, start + 2)) - 1));
+            decode(num, start + 2, new StringBuilder(stringBuilder), list);
+        }
     }
 }
