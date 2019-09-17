9eab233926570b40d8ae671723c7be085632632b
zhaojinbo <zhaojb1995@163.com>
2019-09-17 10:10:08 +0800

    add Solution_10

--- src/Solution_10.java
+++ src/Solution_10.java
@@ -3,29 +3,29 @@
  */
 public class Solution_10 {
 
-    private int[][] back;
+    private int[][] dp;
 
     public boolean isMatch(String s, String p) {
-        back = new int[s.length() + 1][p.length() + 1];
-        return dp(0, 0, s, p);
+        dp = new int[s.length() + 1][p.length() + 1];
+        return isMatchCore(0, 0, s, p);
     }
 
-    public boolean dp(int i, int j, String t, String p) {
+    private boolean isMatchCore(int i, int j, String t, String p) {
         if (j == p.length())
             return i == t.length();
-        if (back[i][j] == 1)
+        if (dp[i][j] == 1)
             return false;
 
         boolean firstmatch = i < t.length() && (t.charAt(i) == p.charAt(j) || p.charAt(j) == '.');
 
         boolean ans;
         if ((j + 1 < p.length() && p.charAt(j + 1) == '*'))
-            ans = dp(i, j + 2, t, p) || (firstmatch && dp(i + 1, j, t, p));
+            ans = isMatchCore(i, j + 2, t, p) || (firstmatch && isMatchCore(i + 1, j, t, p));
         else
-            ans = firstmatch && dp(i + 1, j + 1, t, p);
+            ans = firstmatch && isMatchCore(i + 1, j + 1, t, p);
 
         if (!ans)
-            back[i][j] = 1;
+            dp[i][j] = 1;
 
         return ans;
     }
