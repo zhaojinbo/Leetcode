73ad021445da32e47ec92b1f686db08bbde63552
zhaojinbo <zhaojb1995@163.com>
2019-09-19 16:28:54 +0800

    update Solution_21

--- src/Solution_21.java
+++ src/Solution_21.java
@@ -1,12 +1,33 @@
 public class Solution_21 {
     /**
-     * 合并两个有序链表
+     * 递归解法，时间复杂度1ms,99.80%
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode newNode;
+        if (l1.val < l2.val) {
+            newNode = l1;
+            newNode.next = mergeTwoLists(l1.next, l2);
+        } else {
+            newNode = l2;
+            newNode.next = mergeTwoLists(l1, l2.next);
+        }
+        return newNode;
+    }
+
+    /**
+     * 合并两个有序链表，非递归解法
      *
      * @param l1 第一个有序链表的头结点
      * @param l2 第二个有序链表的头结点
      * @return 最终合并后的有序链表
      */
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    public ListNode mergeTwoListsTwo(ListNode l1, ListNode l2) {
         if (l1 == null) {
             return l2;
         }
