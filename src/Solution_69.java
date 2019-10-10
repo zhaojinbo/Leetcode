87deea1883ce97acc141d4a4e9e1e7ce89301104
zhaojinbo <zhaojb1995@163.com>
2019-10-10 20:49:12 +0800

    update Solution_69

--- src/Solution_69.java
+++ src/Solution_69.java
@@ -4,21 +4,23 @@ public class Solution_69 {
             return x;
         }
         int low = 1;
-        int high = x;
-        while (low < high) {
-            int mid = (low + high) / 2;
-            if (x / mid == mid) {
+        int high = x - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            int k = x / mid;
+            if (k == mid) {
                 return mid;
-            } else if (x / mid > mid) {
-                if (x/(mid+1)<(mid+1)) {
-                    return mid;
-                } else {
-                    low = mid;
-                }
+            } else if (k > mid) {
+                low = mid + 1;
             } else {
-                high = mid;
+                high = mid - 1;
             }
         }
-        return low;
+        return low-1;
     }
+
+/*    public static void main(String[] args) {
+        int i = new Solution_69().mySqrt(14);
+        System.out.println(i);
+    }*/
 }
